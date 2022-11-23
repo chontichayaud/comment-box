89a674e4bb84c60b30ac67b8d60613796f6d23f6
chontichayaud <chonticha270335@gmail.com>
2022-11-23 23:09:11 +0000

    comment length 1

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("is work")
+function checkCommentLength() {
+  let comment = document.getElementById("comment").value;
+   if (comment.length > 140) {
+    alert("The comment may have no more than 140 characters");
+  }
+ }
+  
+ function updateCommentLength() {
+  let comment = document.getElementById("comment").value;
+  document.getElementById("commentLength").innerHTML = comment.length.toString();
+   if (comment.length > 140) {
+    document.getElementById("comment").style.borderColor = "red";
+  } else {
+    document.getElementById("comment").style.borderColor = "black";
+  }
+ }
