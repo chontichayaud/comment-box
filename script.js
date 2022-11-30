10ea3f1afb8a53486669033efaa4d29b779af765
chontichayaud <chonticha270335@gmail.com>
2022-11-30 20:54:17 +0000

    dispaly comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("is work")
+console.log("is work");
 function checkCommentLength() {
   let comment = document.getElementById("comment").value;
    if (comment.length > 140) {
@@ -16,12 +16,34 @@ function updateCommentLength() {
   }
 }
  
-function displayComment() {
-  let data = "";
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let usercomment = document.getElementById("comment").value;
-   data = name +"<br>"+ email +"<br>"+ usercomment
+// function displayComment() {
+//   let data = "";
+//   let name = document.getElementById("name").value;
+//   let email = document.getElementById("email").value;
+//   let usercomment = document.getElementById("comment").value;
+//   // console.log("1");
+//   data  = name +"<br>"+ email +"<br>"+ usercomment;
+//   // console.log("2" + data.toString());
+//   document.getElementById("data").innerHTML = data; 
+//   // console.log("3" + data.toString());
+// }
+const form = document.querySelector("form");
+const list = document.querySelector("div");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event){ 
+  console.log("here");
+  event.preventDefault();
+  const name = event.target.elements.userName.value;
+  const email = event.target.elements.userEmail.value;
+  const comment = event.target.elements.userComment.value;
+
+  post = name+email+comment
+
+  const newItem = document.createElement("div");
+  newItem.textContent = post;
+  list.appendChild(newItem);
   
-  document.getElementById("data").innerHTML = data
-}
+  form.reset();
+};
